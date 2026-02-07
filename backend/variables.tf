1996ae25839de99d5cde0ce0437ea10978f9bb70
vadimf18 <g7bwqpjccm@privaterelay.appleid.com>
2026-02-07 04:24:27 -0800

    Create variables.tf

---
+++ backend/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment (e.g., dev, prod)"
+}
